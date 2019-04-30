a56d31e847f11651df4cc8d124cb16ffab868889
github username <github email address>
2019-04-30 18:43:00 +0100

    done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,47 @@
 class Artist
 
   attr_reader :name, :years_experience
-
+@@all =[]
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+def self.all
+  @@all
+end
+
+def paintings
+  Painting.all.select{|painting|painting.artist == self}
+end
+
+def galleries
+  paintings.map{|painting|painting.gallery}
+end
+
+def cities
+paintings.map{|painting|painting.gallery.city}
+end
+
+def self.total_experience
+ @@all.map{|artist|artist.years_experience}.sum
+end
+
+def painting_count
+  paintings.length
+end
+
+def paint_per_year
+(painting_count.to_f/self.years_experience)
+end
+
+ def self.most_prolific
+  @@all.max_by{|artist|artist.paint_per_year}
+ end
+
+ def create_painting(title, price, gallery)
+   Painting.new(title, price, gallery, self)
+end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,18 @@
 require_relative '../config/environment.rb'
 
+monet = Artist.new('claude money', 3)
+warhol = Artist.new('andy warhol', 5)
+okeef = Artist.new('georgia okeef', 10)
+
+tate = Gallery.new('tate', 'london')
+louvre = Gallery.new('louvre','paris')
+
+p1 = Painting.new('lilly', 300, monet,louvre)
+p2 = Painting.new('marilyn', 150, warhol, louvre)
+p3 = Painting.new('flower', 450, okeef, tate)
+p4 = Painting.new('pond', 800, monet, tate)
+
+
 
 
 
